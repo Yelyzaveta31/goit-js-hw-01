71095c07a0bac1a0b183e96fac119ca7dada5a22
Yelyzaveta31 <Didenkoelizabeth@gmail.com>
2024-03-07 11:32:52 +0200

    update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,18 +1,24 @@
-const quantity = 5;
-console.log (quantity);
-const pricePerDroid = 3000;
-console.log (pricePerDroid);
- 
-// Оголошення параметрів 
-function makeTransaction (quantity, pricePerDroid) {
-    console.log (`You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`)
-} ;
+// оголошення функціі та параметрів
 
-// Передача аргументів 
-makeTransaction (5, 3000); 
-makeTransaction (3, 1000);
-makeTransaction (10, 500);
+function makeTransaction (quantity, pricePerDroid) {
+    const totalPrice = quantity * pricePerDroid;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+}
+
+// Виведення функції з різними аргументами
 
 console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000)); // "You ordered 3 droids worth 3000 credits!"
 console.log(makeTransaction(10, 500)); // "You ordered 10 droids worth 5000 credits!"
+
+ 
+// //  Оголошення параметрів 
+//  function makeTransaction (quantity, pricePerDroid) {
+//    console.log (`You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`)
+// } ;
+
+// // // Передача аргументів 
+//  makeTransaction (5, 3000); 
+//   makeTransaction (3, 1000);
+//  makeTransaction (10, 500);
+
