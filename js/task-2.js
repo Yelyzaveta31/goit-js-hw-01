51e01d02d5724bd73e563a1f66e1f9d69a812d17
Yelyzaveta31 <Didenkoelizabeth@gmail.com>
2024-03-07 11:33:09 +0200

    update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -1,20 +1,24 @@
-const country = "Australia";
-console.log (country);
-const price = 120;
-console.log (price);
-const deliveryFee = 50; 
-console.log (deliveryFee);
 
-// оголошення параметрів
+// оголошення функціі та параметрів
+
 function getShippingMessage (country, price, deliveryFee) {
-   console.log (`Shipping to ${country} will cost ${price + deliveryFee} credits`)
-} ;
+   const totalPrice = price + deliveryFee;
+   return `Shipping to ${country} will cost ${totalPrice} credits`;
+} 
 
-// Оголошення аргументів
-getShippingMessage ("Australia", 120, 50) ;
-getShippingMessage ("Germany", 80, 20) ;
-getShippingMessage ("Sweden", 100, 20) ;
+// Виведення функції з різними аргументами
 
 console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
 console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
 console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+
+
+// // оголошення параметрів
+// function getShippingMessage (country, price, deliveryFee) {
+//    console.log (`Shipping to ${country} will cost ${price + deliveryFee} credits`)
+// } ;
+
+// // Оголошення аргументів
+// getShippingMessage ("Australia", 120, 50) ;
+// getShippingMessage ("Germany", 80, 20) ;
+// getShippingMessage ("Sweden", 100, 20) ;
